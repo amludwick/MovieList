0cff3aa2af0d308a99814d05e2b61a5ed5ad5563
amludwick <A.M.Ludwick2@newcastle.ac.uk>
2024-01-24 16:07:30 +0000

    Initial commit

---
+++ basic_window.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# create a root widget
+root = Tk()
+
+root.title("Basic window")
+root.configure(background='yellow')
+root.minsize(200,250)
+root.maxsize(500,500)
+
+
+root.mainloop()
