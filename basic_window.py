2cf577359f8d958c4dda4fccfa204bde7cb77289
amludwick <A.M.Ludwick2@newcastle.ac.uk>
2024-01-24 16:27:25 +0000

    Playing around with labels

--- basic_window.py
+++ basic_window.py
@@ -4,9 +4,15 @@ from tkinter import *
 root = Tk()
 
 root.title("Basic window")
-root.configure(background='yellow')
+root.configure(background='grey')
 root.minsize(200,250)
 root.maxsize(500,500)
+root.geometry("300x300+50+50")
 
+text = Label(root, text="movies")
+text.pack()
+
+text2 = Label(root, text="- AML")
+text2.pack()
 
 root.mainloop()
