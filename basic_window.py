f12a2e95ac078940b2c43f52817300f7f8761eb4
amludwick <A.M.Ludwick2@newcastle.ac.uk>
2024-01-26 00:46:57 +0000

    Adding images and placing

--- basic_window.py
+++ basic_window.py
@@ -1,4 +1,6 @@
+import tkinter
 from tkinter import *
+from PIL import ImageTk, Image
 
 # create a root widget
 root = Tk()
@@ -6,7 +8,7 @@ root = Tk()
 root.title("Basic window")
 root.configure(background='grey')
 root.minsize(200,250)
-root.maxsize(500,500)
+root.maxsize(1000,1000)
 root.geometry("300x300+50+50")
 
 text = Label(root, text="movies")
@@ -14,5 +16,15 @@ text.pack()
 
 text2 = Label(root, text="- AML")
 text2.pack()
+
+image1 = Image.open("Posters/Django_Unchained.jpg")
+image1 = image1.resize((300, 450))
+poster = ImageTk.PhotoImage(image1)
+label1 = tkinter.Label(image=poster)
+
+label1.place(x=200, y=200)
+
+#Label(root, text = 'Position image on button')
+Button(root, text = 'hello').pack(side= TOP)
 
 root.mainloop()
